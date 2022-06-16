0a78393d162250336b519dc0a557f2c385e641a5
Luke Vance <lukevance90@gmail.com>
2022-06-16 19:45:32 -0400

    updated external links and readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,11 +34,11 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/lukealanv`,
           },
           {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/lukevance/`,
           },
         ],
       },
