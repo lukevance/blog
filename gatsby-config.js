155319e2be0af8074c5c97eeeeef803fa10c5f73
Luke Vance <lukevance90@gmail.com>
2022-06-16 19:31:15 -0400

    updated about page

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,11 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `Luke Blog`,
+    siteTitleAlt: `Luke Blog - Minimal Blog - Gatsby Theme`,
+    siteHeadline: `Luke Blog - based on a Gatsby Theme from @lekoarts`,
+    siteUrl: `https://www.provolove.com`,
+    siteDescription: `Just my thoughts on tech 43rfgt`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
     author: `@lekoarts_de`,
